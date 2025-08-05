f8cc0a539dea763a359d5c7cc01eb4dde5fcad93
Zack Golden <ZackGolden@pm.me>
2025-08-05 14:37:46 -0400

    Adding Poetry

---
+++ 2024/day15/day15_test.py
@@ -0,0 +1,26 @@
+from day15 import part_1, part_2, parse_lines
+
+def test_parse_lines():
+    lines = [
+        "########",
+        "#..O.O.#",
+        "##@.O..#",
+        "#...O..#",
+        "#.#.O..#",
+        "#...O..#",
+        "#......#",
+        "########",
+        "",
+        "<^^>>>vv<v>>v<<",
+    ]
+
+
+def test_part1():
+  lines = [""]
+  result = part_1(lines)
+  assert 0 == result
+
+def test_part2():
+  lines = [""]
+  result = part_2(lines)
+  assert 0 == result
